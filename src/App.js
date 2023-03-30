6ac386b753723e3d30c7cb6e3c1fa4e89d08d134
Elvan Hidayat <elvanhd@gmail.com>
2023-03-30 17:34:03 +0700

    links and nav-links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 // page components
@@ -12,6 +12,14 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>My Articles</h1>
+          {/* <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link> */}
+          <NavLink exact to='/'>
+            Home
+          </NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
         </nav>
 
         <Switch>
