4076dfe1b5a4ddbaabbb410136d777745d2237be
Elvan Hidayat <elvanhd@gmail.com>
2023-03-30 17:18:55 +0700

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+
+// page components
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/contact'>
+          <Contact />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
